85659fae2df1a9b0afb8b5237b5c4c50b229adf6
Erik Szabó <erik1szabo1@gmail.com>
2024-07-03 17:30:20 +0200

    Handle more AuraSkills LootDropEvent.Cause types

--- src/main/java/gg/auroramc/collections/listener/AuraSkillsListener.java
+++ src/main/java/gg/auroramc/collections/listener/AuraSkillsListener.java
@@ -23,15 +23,15 @@ public class AuraSkillsListener implements Listener {
         var typeId = TypeId.from(item.getType());
 
         switch (e.getCause()) {
-            case FARMING_LUCK -> manager.progressCollections(e.getPlayer(), typeId, item.getAmount(), Trigger.HARVEST);
+            case FARMING_LUCK, FARMING_OTHER_LOOT -> manager.progressCollections(e.getPlayer(), typeId, item.getAmount(), Trigger.HARVEST);
             case FISHING_LUCK, TREASURE_HUNTER, EPIC_CATCH, FISHING_OTHER_LOOT ->
                     manager.progressCollections(e.getPlayer(), typeId, item.getAmount(), Trigger.FISH);
             case FORAGING_LUCK, FORAGING_OTHER_LOOT, MINING_LUCK, EXCAVATION_OTHER_LOOT, LUCKY_SPADES,
-                 MINING_OTHER_LOOT, EXCAVATION_LUCK ->
+                 MINING_OTHER_LOOT, EXCAVATION_LUCK, METAL_DETECTOR ->
                     manager.progressCollections(e.getPlayer(), typeId, item.getAmount(), Trigger.BLOCK_LOOT);
-            case LUCK_DOUBLE_DROP -> {
-                manager.progressCollections(e.getPlayer(), typeId, item.getAmount(), Trigger.HARVEST, Trigger.FISH, Trigger.BLOCK_LOOT);
-            }
+            case MOB_LOOT_TABLE -> manager.progressCollections(e.getPlayer(), typeId, item.getAmount(), Trigger.ENTITY_LOOT);
+            case LUCK_DOUBLE_DROP ->
+                    manager.progressCollections(e.getPlayer(), typeId, item.getAmount(), Trigger.HARVEST, Trigger.FISH, Trigger.BLOCK_LOOT, Trigger.ENTITY_LOOT);
         }
     }
 }
